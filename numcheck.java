96cf2b101e411bc951f5d0177b5bf2e6f6efeeee
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-12-16 16:26:53 +0900

    Create numcheck.java

---
+++ numcheck.java
@@ -0,0 +1,8 @@
+    public static Integer checkNum(char x){
+        if(x == '0' || x == '1'|| x == '2'|| x == '3'|| x == '4'|| x == '5'|| x == '6'|| x == '7'|| x == '8'|| x == '9'){
+            return 0;
+        }else{
+            return -1;
+        }
+    }
+  
